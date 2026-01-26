9483914d6c261d1dfbc5bbe8c7b54a8f1e1cae51
theo-watcha <theo@watcha.com>
2026-01-26 14:34:29 +0900

    fix: project attachment 스펙 변경

--- packages/react-event-callback/project-attachment/post-build.sh
+++ packages/react-event-callback/project-attachment/post-build.sh
@@ -3,30 +3,7 @@ set -e
 
 echo "Post build 스크립트가 실행 됩니다."
 
-echo "📁 Copy d.ts in src"
-find "src" -type f -name "*.d.ts" | while read -r FILE; do
-  # 대상 파일의 상대 경로
-  RELATIVE_PATH="${FILE#src/}"
-  TARGET="dist/type/$RELATIVE_PATH"
-
-  echo "$RELATIVE_PATH → ${TARGET}"
-  mkdir -p "$(dirname "$TARGET")"
-  cp "$FILE" "$TARGET"
-
-  for ADDITIONAL_MODULE in "dist/esm" "dist/cjs"; do
-    JS_PATH=${RELATIVE_PATH%.d.ts}.js
-    JS_TARGET="${ADDITIONAL_MODULE}/${JS_PATH}"
-    if [ ! -f "$JS_TARGET" ]; then
-      mkdir -p "$(dirname "$JS_TARGET")"
-      if [ "$ADDITIONAL_MODULE" = "dist/cjs" ]; then
-        echo "module.exports = {};" > "$JS_TARGET"
-      else
-        echo "export {};" > "$JS_TARGET"
-      fi
-      echo "$RELATIVE_PATH → ${JS_TARGET}"
-    fi
-  done
-done
-echo ""
+bash ./project-attachment/copy-dts.sh
+bash ./project-attachment/fix-import.sh
 
 echo "✅ Post build 스크립트가 실행 완료 되었습니다."
