c3ac36ce26f79257ac5783459d09e60bd2de8ae0
theo-watcha <theo@watcha.com>
2026-02-11 17:56:54 +0900

    feat: option 분리

---
+++ packages/react-a11y/src/script/type/accessibility-type.ts
@@ -0,0 +1,13 @@
+export type UseAccessibilityHandlerOptions<ElementType extends HTMLElement> = {
+  /**
+   * - 포커스 복귀 대상 element 의 ref 값입니다.
+   */
+  returnTarget?:
+    | Document
+    | React.RefObject<ElementType | null>
+    | "last-active-element";
+  /**
+   * - 자동 포커스 여부 (기본값: false)
+   */
+  withAutoFocus?: boolean;
+};
