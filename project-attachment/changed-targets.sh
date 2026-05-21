fe8f392d6d67e93b15ec23f78d78823307b60826
theo-watcha <theo@watcha.com>
2026-05-21 10:18:58 +0900

    fix: pnpm exec (compat)

--- project-attachment/changed-targets.sh
+++ project-attachment/changed-targets.sh
@@ -2,7 +2,7 @@
 # 변경된 패키지를 반환합니다.
 
 set +e
-lerna changed --parseable | while read -r PACKAGE_PATH; do
+pnpm exec lerna changed --parseable | while read -r PACKAGE_PATH; do
   if [ -d "$PACKAGE_PATH" ]; then
     echo "$PACKAGE_PATH"
   fi
